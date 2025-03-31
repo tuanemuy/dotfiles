2cec43d6755aa83160750e0f79fd95e03034dea1
tuanemuy <22880537+tuanemuy@users.noreply.github.com>
2025-04-01 01:00:12 +0900

    Fix bugs

--- tools/change-theme/run.sh
+++ tools/change-theme/run.sh
@@ -9,13 +9,13 @@ weztermConfig="$GIT_DIRECTORY/dotfiles/config/wezterm.lua"
 ghosttyConfig="$GIT_DIRECTORY/dotfiles/config/ghostty.config"
 
 if [ "$theme" = "light" ]; then
-  sed -i '' 's/color_scheme[^\S]*=.*/color_scheme = "Gruvbox light, medium (base16)"/g' $weztermConfig
-  sed -i '' 's/theme[^\S]*=.*/theme = GruvboxLight/g' $ghosttyConfig
+  sed -i.bak 's/color_scheme[^\S]*=.*/color_scheme = "Gruvbox light, medium (base16)"/g' $weztermConfig && rm "$weztermConfig.bak"
+  sed -i.bak 's/theme[^\S]*=.*/theme = GruvboxLight/g' $ghosttyConfig && rm "$ghosttyConfig.bak"
 fi
 
 if [ "$theme" = "dark" ]; then
-  sed -i '' 's/color_scheme[^\S]*=.*/color_scheme = "Gruvbox dark, medium (base16)"/g' $weztermConfig
-  sed -i '' 's/theme[^\S]*=.*/theme = GruvboxDark/g' $ghosttyConfig
+  sed -i.bak 's/color_scheme[^\S]*=.*/color_scheme = "Gruvbox dark, medium (base16)"/g' $weztermConfig && rm "$weztermConfig.bak"
+  sed -i.bak 's/theme[^\S]*=.*/theme = GruvboxDark/g' $ghosttyConfig && rm "$ghosttyConfig.bak"
 fi
 
 echo $theme
