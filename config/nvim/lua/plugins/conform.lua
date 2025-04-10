31101705a45afa7e07ee32e873e1341e5b587400
tuanemuy <22880537+tuanemuy@users.noreply.github.com>
2025-04-10 18:34:16 +0900

    feat: Use config file

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,7 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 	keys = {
 		{
 			"<leader>f",
@@ -10,6 +11,45 @@ return {
 			mode = "",
 		},
 	},
+	config = function()
+		require("conform").setup({
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+			formatters = {
+				markdownlint_cli2 = {
+					command = "markdownlint-cli2",
+					stdin = false,
+					args = { "--fix", "$FILENAME" },
+					exit_codes = { 0, 1 },
+					cwd = require("conform.util").root_file({
+						".markdownlint.json",
+						".markdownlint-cli2.json",
+						".markdownlint.jsonc",
+						".markdownlint-cli2.jsonc",
+					}),
+				},
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				bash = { "shfmt" },
+				typescript = { "biome" },
+				javascript = { "biome" },
+				typescriptreact = { "biome" },
+				javascriptreact = { "biome" },
+				json = { "biome" },
+				jsonc = { "biome" },
+				yaml = { "prettierd" },
+				html = { "prettierd" },
+				css = { "biome" },
+				scss = { "biome" },
+				markdown = { "markdownlint_cli2" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				python = { "ruff_format" },
+				nix = { "nixfmt" },
+			},
+		})
+	end,
 	opts = {
 		default_format_opts = {
 			lsp_format = "fallback",
@@ -27,7 +67,7 @@ return {
 			html = { "prettierd" },
 			css = { "biome" },
 			scss = { "biome" },
-			markdown = { "markdownlint-cli2" },
+			markdown = { "markdownlint_cli2" },
 			rust = { "rustfmt", lsp_format = "fallback" },
 			python = { "ruff_format" },
 			nix = { "nixfmt" },
